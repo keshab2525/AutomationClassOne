746e7090712e00f4df43891fa9a7870cfe9e7169
KESHAB KURI <KESHAB KURI@DESKTOP-P7BC3MT>
2020-02-02 16:51:24 -0500

    mother

---
+++ AutomationClassOne/src/main/java/com/DataTypePackage/DataType.java
@@ -0,0 +1,36 @@
+package com.DataTypePackage;
+
+public class DataType {
+// this is another line comment
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+int x=1;
+int y=2;
+int i=3;
+x=5;
+x=2;
+// double data type
+double a=1.5;		
+		a=2.1;
+		
+		
+		// character data type
+		char z = 'z';
+		// string is a non primitive data type. string its a class.
+		String name = "shomir";
+		
+		boolean b= true;
+		boolean c= false;
+	
+   System.out.println("my name is"+ name); 	
+   
+				
+				
+	}
+
+ public static void addtion() {
+
+ }   
+
+}
